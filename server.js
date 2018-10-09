bb55d4ef948cf9a883ba6fbc8010b22e811e5d27
Yvette <yvettetran@gmail.com>
2018-10-08 21:23:13 -0700

    cleaned server code

--- server.js
+++ server.js
@@ -4,23 +4,10 @@ const routes = require('./routes');
 
 const port = process.env.port || 8080;
 
-const request = require('request');
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// app.use(routes);
-
-app.get('/search/:zip', (req, res) => {
-	request('https://api.openweathermap.org/data/2.5/weather?APPID=18d1f1f1d028e3a40481786d7404c7d3&zip=95112', (error, response, body) => {
-			if(error) {
-				console.log('ERROR: ', error);
-			}
-			console.log(JSON.parse(body).main.temp)
-	})
-})
+app.use(routes);
 
 app.listen(port, (error) => {
 	if(error) {
