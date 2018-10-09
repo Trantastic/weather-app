8df519fcaf79aac3654515076a33510580784329
Yvette <yvettetran@gmail.com>
2018-10-09 11:59:27 -0700

    fixed spacing

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Search = (props) => (
 	<div>
-	<span className='mx-auto input-group mt-5 w-25'>
-		<input className="form-control" type="search" placeholder="Enter a zipcode" aria-label="Search" onChange = { props.handleInputChange } />
-		<button className="btn btn-warning ml-2" type="submit" onClick={ props.getWeather }>Search</button>
-	</span>
+		<span className='mx-auto input-group mt-5 w-25'>
+			<input className="form-control" type="search" placeholder="Enter a zipcode" aria-label="Search" onChange = { props.handleInputChange } />
+			<button className="btn btn-warning ml-2" type="submit" onClick={ props.getWeather }>Search</button>
+		</span>
 	</div>
 )
 
