97aaf7e760dd363741db8d9e553bd6a72af36e59
Yvette <yvettetran@gmail.com>
2018-10-08 21:29:43 -0700

    fixed request code for weatherapi route

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,13 @@ const url = weatherAPI.url;
 
 router.route('/search/:zip')
 	.get((req, res) => {
-		url = JSON.stringify(url + req.params.zip);
-		// console.log(JSON.stringify(url + req.params.zip))
-		request('https://api.openweathermap.org/data/2.5/weather?APPID=18d1f1f1d028e3a40481786d7404c7d3&zip=95112', (error, response, body) => {
+		request(url + req.params.zip, (error, response, body) => {
 			if(error) {
 				console.log('ERROR: ', error);
 			}
 			console.log(JSON.parse(body).main.temp)
+			res.json(JSON.parse(body).main.temp);
 		})
-	// 	// console.log('REQ', req.params.zip)
 	})
 
 module.exports = router;
